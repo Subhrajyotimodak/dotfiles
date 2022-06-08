875df32c8f8afceea993b674f00c997957a357a4
Subhrajyotimodak <writer1ani@gmail.com>
2022-06-08 23:46:58 +0530

    Markdown and FloaTerm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,7 +47,7 @@ return packer.startup(function(use)
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use "kyazdani42/nvim-web-devicons"
-  use  "kyazdani42/nvim-tree.lua"
+  use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
   use "nvim-lualine/lualine.nvim"
@@ -64,12 +64,12 @@ return packer.startup(function(use)
   use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
-use "hrsh7th/nvim-cmp" -- The completion plugin
-use "hrsh7th/cmp-buffer" -- buffer completions
- use "hrsh7th/cmp-path" -- path completions
- use "hrsh7th/cmp-cmdline" -- cmdline completions
- use "saadparwaiz1/cmp_luasnip" -- snippet completions
- use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
@@ -94,6 +94,21 @@ use "hrsh7th/cmp-buffer" -- buffer completions
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+
+  -- Terminal
+  use 'voldikss/vim-floaterm'
+
+
+
+  -- For Markdown and Writing
+  use {
+    "Pocco81/TrueZen.nvim",
+    ft = { 'markdown', 'txt' }
+  }
+  use {
+    "folke/zen-mode.nvim",
+    ft = { 'markdown', 'txt' }
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
